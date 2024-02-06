00f1bc420eecb2bacede9672ef8fe5a1a24e84a5
Tassos Marinos <mtassos@gmail.com>
2024-02-06 20:16:05 +0200

    Happy New Year

--- source/plugins/system/tgeoip/script.install.php
+++ source/plugins/system/tgeoip/script.install.php
@@ -2,7 +2,7 @@
 /**
  * @author          Tassos Marinos <info@tassos.gr>
  * @link            http://www.tassos.gr
- * @copyright       Copyright © 2023 Tassos Marinos All Rights Reserved
+ * @copyright       Copyright © 2024 Tassos Marinos All Rights Reserved
  * @license         GNU GPLv3 <http://www.gnu.org/licenses/gpl.html> or later
 */
 
